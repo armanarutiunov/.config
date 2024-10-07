a647bec6cf10c4733574595fb16af33b909b28bd
Arman Arutiunov <arman.arutiunov@gmail.com>
2024-10-08 00:35:37 +0100

    Some changes, blah

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,29 +1,79 @@
 return {
-    'shaunsingh/nord.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        -- Example config in lua
-        vim.g.nord_contrast = true
-        vim.g.nord_borders = false
-        vim.g.nord_disable_background = true
-        vim.g.nord_italic = false
-        vim.g.nord_uniform_diff_background = true
-        vim.g.nord_bold = false
+	--    'shaunsingh/nord.nvim',
+	"EdenEast/nightfox.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		local config = {
+			options = {
+				-- Compiled file's destination location
+				compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+				compile_file_suffix = "_compiled", -- Compiled file suffix
+				transparent = true,    -- Disable setting background
+				terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+				dim_inactive = false,  -- Non focused panes set to alternative background
+				module_default = true, -- Default enable value for modules
+				colorblind = {
+					enable = false,    -- Enable colorblind support
+					simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
+					severity = {
+						protan = 0,    -- Severity [0,1] for protan (red)
+						deutan = 0,    -- Severity [0,1] for deutan (green)
+						tritan = 0,    -- Severity [0,1] for tritan (blue)
+					},
+				},
+				styles = { -- Style to be applied to different syntax groups
+					comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+					conditionals = "NONE",
+					constants = "NONE",
+					functions = "NONE",
+					keywords = "NONE",
+					numbers = "NONE",
+					operators = "NONE",
+					strings = "NONE",
+					types = "NONE",
+					variables = "NONE",
+				},
+				inverse = { -- Inverse highlight for different types
+					match_paren = false,
+					visual = false,
+					search = false,
+				},
+			},
+			palettes = {},
+			specs = {},
+			groups = {},
+		}
+		-- Example config in lua
+		--	vim.cmd("colorscheme duskfox")
+		-- Load the colorscheme
+		--        require('nord').set()
 
-        -- Load the colorscheme
-        require('nord').set()
+		-- Toggle background transparency
+		--
+		local onedark = require("nightfox")
+		onedark.setup(config)
+		onedark.load()
 
-        -- Toggle background transparency
+		-- Make the background of diagnostics messages transparent
+		local set_diagnostics_bg_transparency = function()
+			vim.cmd([[highlight DiagnosticVirtualTextError guibg=none]])
+			vim.cmd([[highlight DiagnosticVirtualTextWarn guibg=none]])
+			vim.cmd([[highlight DiagnosticVirtualTextInfo guibg=none]])
+			vim.cmd([[highlight DiagnosticVirtualTextHint guibg=none]])
+		end
+		set_diagnostics_bg_transparency()
 
-        local bg_transparent = true
+		-- Toggle background transparency
+		local toggle_transparency = function()
+			config.transparent = not config.transparent
+			onedark.setup(config)
+			onedark.load()
+			set_diagnostics_bg_transparency()
+		end
 
-        local toggle_transparency = function()
-            bg_transparent = not bg_transparent
-            vim.g.nord_disable_background = bg_transparent
-            vim.cmd [[colorscheme nord]]
-        end
+		vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
 
-        vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
-    end
+		vim.cmd("colorscheme duskfox")
+	end,
 }
