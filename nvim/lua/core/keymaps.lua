6dc86dd0f0672b71a2d503c49a677a348464053b
Arman Arutiunov <arman.arutiunov@gmail.com>
2024-10-15 19:40:55 +0100

    rest plugin

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -51,10 +51,19 @@ vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
 vim.keymap.set("n", "<leader>+", "<C-a>", opts) -- increment
 vim.keymap.set("n", "<leader>-", "<C-x>", opts) -- decrement
 
+-- Rest API plugin
+vim.keymap.set("n", "<leader>Ho", ":Rest open", opts)
+vim.keymap.set("n", "<leader>Hr", ":Rest run", opts)
+vim.keymap.set("n", "<leader>Hl", ":Rest last", opts)
+vim.keymap.set("n", "<leader>Hlg", ":Rest logs", opts)
+vim.keymap.set("n", "<leader>Hc", ":Rest cookies", opts)
+vim.keymap.set("n", "<leader>Hes", ":Rest env show", opts)
+vim.keymap.set("n", "<leader>Hesl", ":Rest env select", opts)
+
 -- window management
-vim.keymap.set("n", "<leader>v", "<C-w>v", opts)      -- split window vertically
-vim.keymap.set("n", "<leader>h", "<C-w>s", opts)      -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", opts)     -- make split windows equal width & height
+vim.keymap.set("n", "<leader>v", "<C-w>v", opts) -- split window vertically
+vim.keymap.set("n", "<leader>h", "<C-w>s", opts) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width & height
 vim.keymap.set("n", "<leader>xs", ":close<CR>", opts) -- close current split window
 
 -- Navigate between splits
@@ -64,12 +73,12 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
 
 -- tabs
-vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts)    -- open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts)  -- close current tab
-vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts)      --  go to next tab
-vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts)      --  go to previous tab
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) --  go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) --  go to previous tab
 
-vim.keymap.set("n", "<leader>x", ":Bdelete!<CR>", opts)   -- close buffer
+vim.keymap.set("n", "<leader>x", ":Bdelete!<CR>", opts) -- close buffer
 vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts) -- new buffer
 
 -- toggle line wrapping
@@ -101,13 +110,13 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 local diagnostics_active = true
 
 vim.keymap.set("n", "<leader>do", function()
-  diagnostics_active = not diagnostics_active
+	diagnostics_active = not diagnostics_active
 
-  if diagnostics_active then
-    vim.diagnostic.enable(true)
-  else
-    vim.diagnostic.enable(false)
-  end
+	if diagnostics_active then
+		vim.diagnostic.enable(true)
+	else
+		vim.diagnostic.enable(false)
+	end
 end)
 
 -- Diagnostic keymaps
