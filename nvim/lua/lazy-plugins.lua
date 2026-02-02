0ca2ffe21c743fbbdc886836f56dc749393ca379
armanarutiunov <arman.arutiunov@fresha.com>
2026-02-02 14:50:01 +0000

    Lazygit nvim plugin

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -64,6 +64,8 @@ require('lazy').setup({
   -- require 'plugins.autopairs',
   require 'plugins.neo-tree',
 
+  require 'plugins.lazygit',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
